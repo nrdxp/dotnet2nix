be486cf5abbd8e2c4a8f04cbe8b601944d3bf8e0
Timothy DeHerrera <tim.deh@pm.me>
2020-12-15 20:35:40 -0700

    flake: add overlay

--- flake.nix
+++ flake.nix
@@ -1,10 +1,13 @@
 {
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in
     {
       packages.x86_64-linux = import ./default.nix { inherit pkgs; };
+      overlay = final: prev: {
+        dotnet2nix = self.packages.x86_64-linux.dotnet2nix;
+      };
     };
 }
